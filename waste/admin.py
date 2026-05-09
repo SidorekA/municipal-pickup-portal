9aba20585f86fc61cc045664b459ed72cd62cdee
Adrian Sidorek <adriansidorek@gmail.com>
2026-05-09 23:31:55 +0200

    feat(admin): add WasteCost inline to WasteFraction admin and update model reference in WasteCost

--- waste/admin.py
+++ waste/admin.py
@@ -1,7 +1,7 @@
 # waste/admin.py
 
 from django.contrib import admin
-from .models import WasteFractionType, WasteFraction
+from .models import WasteCost, WasteFractionType, WasteFraction
 # Register your models here.
 
 class WasteFractionInline(admin.TabularInline):
@@ -9,9 +9,21 @@ class WasteFractionInline(admin.TabularInline):
     extra = 1
     fields = ['capacity', 'unit', 'active']
 
+class WasteCostInline(admin.TabularInline):
+    model = WasteCost
+    extra = 1
+    fields = ['cost', 'date_from', 'date_to']
+
 @admin.register(WasteFractionType)
 class WasteFractionTypeAdmin(admin.ModelAdmin):
     list_display = ['code', 'name', 'active']
     list_filter = ['active']
     search_fields = ['name', 'code']
     inlines = [WasteFractionInline]
+
+@admin.register(WasteFraction)
+class WasteFractionAdmin(admin.ModelAdmin):
+    list_display = ['fraction_type', 'capacity', 'unit', 'active']
+    list_filter = ['active']
+    search_fields = ['fraction_type', 'capacity']
+    inlines = [WasteCostInline]
